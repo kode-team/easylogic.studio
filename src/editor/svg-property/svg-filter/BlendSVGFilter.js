4bd97e6bb880717fc6d762e8e84a143959216591
easylogic <cyberuls@gmail.com>
2020-06-07 15:54:05 +0900

    fixed svg filter item view

--- src/editor/svg-property/svg-filter/BlendSVGFilter.js
+++ src/editor/svg-property/svg-filter/BlendSVGFilter.js
@@ -1,6 +1,5 @@
 import { BaseSVGFilter } from "./BaseSVGFilter";
 import { OBJECT_TO_PROPERTY } from "../../../util/functions/func";
-import { blend_list } from "../../util/Resource";
  
 export class BlendSVGFilter extends BaseSVGFilter {
   getDefaultObject() {
