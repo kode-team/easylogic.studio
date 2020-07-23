94682fed1f9030ace69d3cd38f47976a52574452
easylogic <cyberuls@gmail.com>
2020-07-24 00:14:32 +0900

    사파리에서 text 선택안되는 문제 수정

--- src/editor/items/layers/TextLayer.js
+++ src/editor/items/layers/TextLayer.js
@@ -60,7 +60,7 @@ export class TextLayer extends Layer {
   get html () {
     var {id, itemType, content} = this.json;
 
-    return /*html*/`<p class='element-item ${itemType}' contenteditable="true" data-id="${id}">${content}</p>`
+    return /*html*/`<p class='element-item ${itemType}' contenteditable="true" tabIndex="-1" data-id="${id}">${content}</p>`
   }
 
 
