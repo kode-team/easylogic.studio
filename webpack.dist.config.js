f7c79cbbcaa4232e39e4ea4f6b40ee50c458ce60
easylogic <cyberuls@gmail.com>
2021-06-29 03:54:16 +0900

    update dist file

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -43,7 +43,6 @@ module.exports = {
     libraryExport : "default",    
     path: __dirname + "/dist",
     filename: '[name].js',
-    publicPath: '/',
     auxiliaryComment: LICENSE
   },
   resolve: { alias },  
