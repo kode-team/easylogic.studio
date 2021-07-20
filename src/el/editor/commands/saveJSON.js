9dca6e09acfc9a255dfd677bc0b5dd01486fd3e6
easylogic <cyberuls@gmail.com>
2021-07-21 01:51:56 +0900

    Refactoring....

--- src/el/editor/commands/saveJSON.js
+++ src/el/editor/commands/saveJSON.js
@@ -1,6 +1,7 @@
 export default {
     command: 'saveJSON',
     execute: function (editor) {
+        // todo: 문서 표준 포맷을 정해야함 
         editor.saveResource('projects', editor.projects)
     }    
 }
