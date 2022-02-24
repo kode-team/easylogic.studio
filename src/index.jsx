29cd1968d11593ed9a8b051e2602f0f8d69a0cb6
easylogic <cyberuls@gmail.com>
2022-02-25 01:41:47 +0900

    support multi editor type

--- src/index.jsx
+++ src/index.jsx
@@ -8,59 +8,6 @@ function startEditor() {
 
     return EasyLogic.createDesignEditor({
       container: document.getElementById(id),
-      onChange: (editor, key, value) => {
-        console.log(editor.getValue(), key, value);
-      },
-      inspector: [
-        {
-          editor: 'Button',
-          editorOptions: {
-            text: 'Edit',
-            onClick: () => {
-              console.log('a');
-            }
-          },
-        },
-        {
-          type: 'column',
-          size: [1, 1],
-          columns: [
-            {
-              key: 'column1',
-              editor: 'color-view',
-              editorOptions: {
-                label: 'Color',
-              }
-            }
-          ]
-        },
-        {
-          type: 'folder',
-          label: 'folder test',
-          children: [
-            {
-              key: 'column1',
-              editor: 'color-view',
-              editorOptions: {
-                label: 'Color',
-              }
-            },
-            {
-              type: 'column',
-              size: [1, 1],
-              columns: [
-                {
-                  key: 'column1',
-                  editor: 'color-view',
-                  editorOptions: {
-                    label: 'Color',
-                  }
-                }
-              ]
-            }
-          ]
-        }
-      ],
       config: {
         'editor.theme': 'light',
         // 'editor.layout.mode': 'svg',
