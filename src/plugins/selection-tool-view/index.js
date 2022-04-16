ebfe2fa71ecba735c5b3ec7170130556480cad5b
easylogic <cyberuls@gmail.com>
2022-04-17 00:32:04 +0900

    support hot module reload support preload check (for plugin loading)

--- src/plugins/selection-tool-view/index.js
+++ src/plugins/selection-tool-view/index.js
@@ -1,7 +1,7 @@
 import { Editor } from "el/editor/manager/Editor";
 import SelectionToolView from './SelectionToolView';
 import GroupSelectionToolView from './GroupSelectionToolView';
-// import GhostToolView from './GhostToolView';
+import GhostToolView from './GhostToolView';
 import { CanvasViewToolLevel } from "el/editor/types/editor";
 
 
@@ -10,8 +10,6 @@ import { CanvasViewToolLevel } from "el/editor/types/editor";
  * @param {Editor} editor 
  */
 export default async function (editor) {
-
-    const GhostToolView = (await import("./GhostToolView")).default;
     editor.registerUI('canvas.view', {
         GhostToolView,
         SelectionToolView,
